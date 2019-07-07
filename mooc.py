6422d09d75af4ba97b19d6ae51ed0639f85261d8
acheamponge <acheamponge@berea.edu>
2019-07-07 05:37:30 -0400

    added new modules create new class

--- mooc.py
+++ mooc.py
@@ -3,16 +3,24 @@ import csv
 
 ''' This is a python module for the online learning community.'''
 
-df_blp = pd.read_csv('learning_path_bus.csv')
+class Linkedin_lp_business(object):
 
-def linkedin_learning_path_business():
-    '''This is a method that returns a CSV file of all the learning paths in the business category.'''
-    return(df_blp)
+    def __init__(self):
+        self.df_blp = pd.read_csv('learning_path_bus.csv')
 
-def linkedin_learning_path_business_list():
-    '''This is a method that returns a list of all the learning paths in the business category.'''
-    return(df_blp['name'].tolist())
+    def csv(self):
+        '''This is a method that returns a CSV file of all the learning paths in the business category.'''
+        return(self.df_blp)
 
-def linkedin_learning_path_business_skills():
-    '''This is a method that returns a list of all the learning paths in the business category.'''
-    return(df_blp.groupby('skills').count().index.tolist())
+    def courseList(self):
+        '''This is a method that returns a list of all the learning paths in the business category.'''
+        return(self.df_blp['name'].tolist())
+
+    def skillsList(self):
+        '''This is a method that returns a list of all the skills in the business category.'''
+        return(self.df_blp.groupby('skills').count().index.tolist())
+
+    def skillsdict(self):
+        '''This is a method that returns a dictionary of skills and their learning paths in the business category.'''
+        return(self.df_blp.groupby('skills')['name'].apply(list).to_dict())
+    
