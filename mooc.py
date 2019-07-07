62635bdc3c8f4f84284e97dcbb3e0c3ce5e71327
Emmanuel Acheampong <acheamponge@berea.edu>
2019-07-07 05:00:44 -0400

    Update mooc.py

--- mooc.py
+++ mooc.py
@@ -3,16 +3,16 @@ import csv
 
 ''' This is a python module for the online learning community.'''
 
-df = pd.read_csv('learning_path_bus.csv')
+df_blp = pd.read_csv('learning_path_bus.csv')
 
 def linkedin_learning_path_business():
     '''This is a method that returns a CSV file of all the learning paths in the business category.'''
-    return(df)
+    return(df_blp)
 
 def linkedin_learning_path_business_list():
     '''This is a method that returns a list of all the learning paths in the business category.'''
-    return(df['name'].tolist())
+    return(df_blp['name'].tolist())
 
 def linkedin_learning_path_business_skills():
     '''This is a method that returns a list of all the learning paths in the business category.'''
-    return(df.groupby('skills').count().index.tolist())
+    return(df_blp.groupby('skills').count().index.tolist())
