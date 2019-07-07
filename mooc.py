25e16f7f893e62cb650fb63bd71f716f2f504b23
acheamponge <acheamponge@berea.edu>
2019-07-07 05:51:15 -0400

    changed some comments

--- mooc.py
+++ mooc.py
@@ -31,17 +31,17 @@ class Linkedin_lp_creative(object):
         self.df_clp = pd.read_csv('learning_path_cre.csv')
 
     def csv(self):
-        '''This is a method that returns a CSV file of all the learning paths in the business category.'''
+        '''This is a method that returns a CSV file of all the learning paths in the creative category.'''
         return(self.df_clp)
 
     def courseList(self):
-        '''This is a method that returns a list of all the learning paths in the business category.'''
+        '''This is a method that returns a list of all the learning paths in the creative category.'''
         return(self.df_clp['name'].tolist())
 
     def skillsList(self):
-        '''This is a method that returns a list of all the skills in the business category.'''
+        '''This is a method that returns a list of all the skills in the creative category.'''
         return(self.df_clp.groupby('skills').count().index.tolist())
 
     def skillsdict(self):
-        '''This is a method that returns a dictionary of skills and their learning paths in the business category.'''
+        '''This is a method that returns a dictionary of skills and their learning paths in the creative category.'''
         return(self.df_clp.groupby('skills')['name'].apply(list).to_dict())
